19818adb837ead635017aa0d9e3a6287287cbcbe
Jan Funke <jfunke@iri.upc.edu>
2016-12-28 16:52:27 -0500

    Block on concurrent compile of same module
    This fixes the remaining race condition if several processes try to
    compile the same module (with the same merge function) at the same time.

--- waterz/__init__.py
+++ waterz/__init__.py
@@ -70,6 +70,7 @@ def agglomerate(
     import shutil
     import glob
     import numpy
+    import fcntl
 
     try:
         import hashlib
@@ -101,76 +102,83 @@ def agglomerate(
     module_name = 'waterz_' + hashlib.md5(str(key).encode('utf-8')).hexdigest()
     lib_dir=os.path.expanduser('~/.cython/inline')
 
+    # since this could be called concurrently, there is no good way to check
+    # whether the directory already exists
     try:
+        os.makedirs(lib_dir)
+    except:
+        pass
+
+    # make sure the same module is not build concurrently
+    with open(os.path.join(lib_dir, module_name + '.lock'), 'w') as lock_file:
+        fcntl.lockf(lock_file, fcntl.LOCK_EX)
 
         try:
-            os.makedirs(lib_dir)
-        except:
-            pass
-        if lib_dir not in sys.path:
-            sys.path.append(lib_dir)
-        if force_rebuild:
-            raise ImportError
-        else:
-            __import__(module_name)
 
-        print("Re-using already compiled waterz version")
+            if lib_dir not in sys.path:
+                sys.path.append(lib_dir)
+            if force_rebuild:
+                raise ImportError
+            else:
+                __import__(module_name)
 
-    except ImportError:
+            print("Re-using already compiled waterz version")
 
-        print("Compiling waterz in " + str(lib_dir))
+        except ImportError:
 
-        cython_include_dirs = ['.']
-        ctx = Context(cython_include_dirs, default_options)
+            print("Compiling waterz in " + str(lib_dir))
 
-        scoring_function_include_dir = os.path.join(lib_dir, module_name)
-        if not os.path.exists(scoring_function_include_dir):
-            os.makedirs(scoring_function_include_dir)
+            cython_include_dirs = ['.']
+            ctx = Context(cython_include_dirs, default_options)
 
-        include_dirs = [
-            source_dir,
-            scoring_function_include_dir,
-            os.path.join(source_dir, 'backend'),
-            os.path.dirname(get_python_inc()),
-            numpy.get_include(),
-        ]
+            scoring_function_include_dir = os.path.join(lib_dir, module_name)
+            if not os.path.exists(scoring_function_include_dir):
+                os.makedirs(scoring_function_include_dir)
 
-        scoring_function_header = os.path.join(scoring_function_include_dir, 'ScoringFunction.h')
-        with open(scoring_function_header, 'w') as f:
-            f.write('typedef %s ScoringFunctionType;'%scoring_function)
+            include_dirs = [
+                source_dir,
+                scoring_function_include_dir,
+                os.path.join(source_dir, 'backend'),
+                os.path.dirname(get_python_inc()),
+                numpy.get_include(),
+            ]
 
-        # cython requires that the pyx file has the same name as the module
-        shutil.copy(
-                os.path.join(source_dir, 'frontend.pyx'),
-                os.path.join(lib_dir, module_name + '.pyx')
-        )
-        shutil.copy(
-                os.path.join(source_dir, 'c_frontend.cpp'),
-                os.path.join(lib_dir, module_name + '_c_frontend.cpp')
-        )
+            scoring_function_header = os.path.join(scoring_function_include_dir, 'ScoringFunction.h')
+            with open(scoring_function_header, 'w') as f:
+                f.write('typedef %s ScoringFunctionType;'%scoring_function)
 
-        # Remove the "-Wstrict-prototypes" compiler option, which isn't valid 
-        # for C++.
-        cfg_vars = get_config_vars()
-        if "CFLAGS" in cfg_vars:
-            cfg_vars["CFLAGS"] = cfg_vars["CFLAGS"].replace("-Wstrict-prototypes", "")
-
-        extension = Extension(
-                module_name,
-                sources = [
-                    os.path.join(lib_dir, module_name + '.pyx'),
+            # cython requires that the pyx file has the same name as the module
+            shutil.copy(
+                    os.path.join(source_dir, 'frontend.pyx'),
+                    os.path.join(lib_dir, module_name + '.pyx')
+            )
+            shutil.copy(
+                    os.path.join(source_dir, 'c_frontend.cpp'),
                     os.path.join(lib_dir, module_name + '_c_frontend.cpp')
-                ],
-                include_dirs=include_dirs,
-                language='c++',
-                extra_link_args=['-std=c++11'],
-                extra_compile_args=['-std=c++11', '-w']
-        )
-        build_extension = build_ext(Distribution())
-        build_extension.finalize_options()
-        build_extension.extensions = cythonize([extension], quiet=True, nthreads=2)
-        build_extension.build_temp = lib_dir
-        build_extension.build_lib  = lib_dir
-        build_extension.run()
+            )
+
+            # Remove the "-Wstrict-prototypes" compiler option, which isn't valid 
+            # for C++.
+            cfg_vars = get_config_vars()
+            if "CFLAGS" in cfg_vars:
+                cfg_vars["CFLAGS"] = cfg_vars["CFLAGS"].replace("-Wstrict-prototypes", "")
+
+            extension = Extension(
+                    module_name,
+                    sources = [
+                        os.path.join(lib_dir, module_name + '.pyx'),
+                        os.path.join(lib_dir, module_name + '_c_frontend.cpp')
+                    ],
+                    include_dirs=include_dirs,
+                    language='c++',
+                    extra_link_args=['-std=c++11'],
+                    extra_compile_args=['-std=c++11', '-w']
+            )
+            build_extension = build_ext(Distribution())
+            build_extension.finalize_options()
+            build_extension.extensions = cythonize([extension], quiet=True, nthreads=2)
+            build_extension.build_temp = lib_dir
+            build_extension.build_lib  = lib_dir
+            build_extension.run()
 
     return __import__(module_name).agglomerate(affs, thresholds, gt, aff_threshold_low, aff_threshold_high)
