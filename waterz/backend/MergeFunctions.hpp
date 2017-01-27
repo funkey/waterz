d4ffd43e204ff0a48772e89aad06c15b916c5277
Jan Funke <jfunke@iri.upc.edu>
2017-01-27 17:19:17 +0100

    Add QuantileAffinity merge function
    The quantile can be given as second template argument as integer between
    0 and 100 (min and max).

--- waterz/backend/MergeFunctions.hpp
+++ waterz/backend/MergeFunctions.hpp
@@ -128,6 +128,83 @@ private:
 };
 
 /**
+ * Scores edges with median affinity.
+ */
+template <typename AffinityMapType, int Quantile>
+class QuantileAffinity {
+
+public:
+
+	typedef typename AffinityMapType::RegionGraphType RegionGraphType;
+	typedef typename AffinityMapType::ValueType       ScoreType;
+	typedef typename RegionGraphType::NodeIdType      NodeIdType;
+	typedef typename RegionGraphType::EdgeIdType      EdgeIdType;
+
+	template <typename SizeMapType>
+	QuantileAffinity(AffinityMapType& affinities, SizeMapType& regionSizes) :
+		_affinities(affinities),
+		_affiliatedEdges(affinities.getRegionGraph()) {}
+
+	/**
+	 * Get the score for an edge. An edge will be merged the earlier, the 
+	 * smaller its score is.
+	 */
+	ScoreType operator()(EdgeIdType e) {
+
+		std::vector<EdgeIdType>& affiliatedEdges = _affiliatedEdges[e];
+
+		// initial edges have their own affinity
+		if (affiliatedEdges.size() == 0)
+			return _affinities[e];
+
+		// edges resulting from merges consult their affiliated edges
+
+		auto quantile = affiliatedEdges.begin() + (affiliatedEdges.size() - 1)*Quantile/100;
+		std::nth_element(
+				affiliatedEdges.begin(),
+				quantile,
+				affiliatedEdges.end(),
+				[this](EdgeIdType a, EdgeIdType b){
+
+					return _affinities[a] < _affinities[b];
+				}
+		);
+
+		return _affinities[*quantile];
+	}
+
+	void notifyNodeMerge(NodeIdType from, NodeIdType to) {}
+
+	void notifyEdgeMerge(EdgeIdType from, EdgeIdType to) {
+
+		if (_affiliatedEdges[from].size() == 0)
+			// 'from' is an initial edge
+			_affiliatedEdges[to].push_back(from);
+		else
+			// 'from' is a compound edge, copy its affiliated edges to the new 
+			// affiliated edge list
+			std::copy(
+					_affiliatedEdges[from].begin(),
+					_affiliatedEdges[from].end(),
+					std::back_inserter(_affiliatedEdges[to]));
+
+		// clear affiliated edges of merged region edges -- they are not 
+		// needed anymore
+		_affiliatedEdges[from].clear();
+	}
+
+private:
+
+	const AffinityMapType& _affinities;
+
+	// for every new edge between regions u and v, the edges of the initial RAG 
+	// between any child of u and any child of v
+	//
+	// initial edges will have this empty
+	typename RegionGraphType::template EdgeMap<std::vector<EdgeIdType>> _affiliatedEdges;
+};
+
+/**
  * Scores edges with max affinity.
  */
 template <typename AffinityMapType>
