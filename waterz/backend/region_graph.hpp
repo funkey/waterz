c9b757709a865c9adc239c0b9c414bbdc9c57e9e
Jan Funke <jfunke@iri.upc.edu>
2017-03-12 17:46:32 +0100

    Ignore label 0 during RAG extraction from labels

--- waterz/backend/region_graph.hpp
+++ waterz/backend/region_graph.hpp
@@ -43,58 +43,45 @@ get_region_graph(
 	std::vector<std::map<ID, EdgeIdType>> edges(max_segid+1);
 
 	EdgeIdType e;
-	for ( std::ptrdiff_t z = 0; z < zdim; ++z )
-		for ( std::ptrdiff_t y = 0; y < ydim; ++y )
-			for ( std::ptrdiff_t x = 0; x < xdim; ++x ) {
+	std::size_t p[3];
+	for (p[0] = 0; p[0] < zdim; ++p[0])
+		for (p[1] = 0; p[1] < ydim; ++p[1])
+			for (p[2] = 0; p[2] < xdim; ++p[2]) {
 
-				statisticsProvider.addVoxel(seg[z][y][x], x, y, z);
+				ID id1 = seg[p[0]][p[1]][p[2]];
+				if (id1 == 0)
+					continue;
 
-				if ((z > 0) && seg[z][y][x] != seg[z-1][y][x]) {
+				statisticsProvider.addVoxel(id1, p[2], p[1], p[0]);
 
-					auto mm = std::minmax(seg[z][y][x], seg[z-1][y][x]);
+				for (int d = 0; d < 3; d++) {
 
-					auto edgeIt = edges[mm.first].find(mm.second);
-					if (edgeIt == edges[mm.first].end()) {
-						e = rg.addEdge(mm.first, mm.second);
-						edges[mm.first][mm.second] = e;
-						statisticsProvider.notifyNewEdge(e);
-					} else {
-						e = edgeIt->second;
+					if (p[d] == 0)
+						continue;
+
+					ID id2 = seg[p[0]-(d==0)][p[1]-(d==1)][p[2]-(d==2)];
+					if (id2 == 0)
+						continue;
+
+					if (id1 != id2) {
+
+						auto mm = std::minmax(id1, id2);
+						auto edgeIt = edges[mm.first].find(mm.second);
+
+						if (edgeIt == edges[mm.first].end()) {
+
+							e = rg.addEdge(mm.first, mm.second);
+							edges[mm.first][mm.second] = e;
+
+							statisticsProvider.notifyNewEdge(e);
+
+						} else {
+
+							e = edgeIt->second;
+						}
+
+						statisticsProvider.addAffinity(e, aff[d][p[0]][p[1]][p[2]]);
 					}
-
-					statisticsProvider.addAffinity(e, aff[0][z][y][x]);
-				}
-
-				if ((y > 0) && seg[z][y][x] != seg[z][y-1][x]) {
-
-					auto mm = std::minmax(seg[z][y][x], seg[z][y-1][x]);
-
-					auto edgeIt = edges[mm.first].find(mm.second);
-					if (edgeIt == edges[mm.first].end()) {
-						e = rg.addEdge(mm.first, mm.second);
-						edges[mm.first][mm.second] = e;
-						statisticsProvider.notifyNewEdge(e);
-					} else {
-						e = edgeIt->second;
-					}
-
-					statisticsProvider.addAffinity(e, aff[1][z][y][x]);
-				}
-
-				if ((x > 0) && seg[z][y][x] != seg[z][y][x-1]) {
-
-					auto mm = std::minmax(seg[z][y][x], seg[z][y][x-1]);
-
-					auto edgeIt = edges[mm.first].find(mm.second);
-					if (edgeIt == edges[mm.first].end()) {
-						e = rg.addEdge(mm.first, mm.second);
-						edges[mm.first][mm.second] = e;
-						statisticsProvider.notifyNewEdge(e);
-					} else {
-						e = edgeIt->second;
-					}
-
-					statisticsProvider.addAffinity(e, aff[2][z][y][x]);
 				}
 			}
 
