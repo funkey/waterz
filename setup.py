39c18d7f2009432bbcc72953cc5b3c1f7d70db4d
jingpengw <jingpeng.wu@gmail.com>
2019-08-15 11:20:49 -0400

    remove dependency of cython and numpy when using setup.py only without the requirements.txt

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from setuptools.extension import Extension
-from Cython.Build import cythonize
-import numpy
+from setuptools.command.build_ext import build_ext as _build_ext
+#from Cython.Build import cythonize
 import os
 
 version = '0.8.2'
@@ -14,17 +14,28 @@ with open('requirements.txt') as f:
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+class build_ext(_build_ext):
+    """We assume no numpy at the begining
+    https://stackoverflow.com/questions/19919905/how-to-bootstrap-numpy-installation-in-setup-py
+    """
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
 source_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'waterz')
 include_dirs = [
     source_dir,
     os.path.join(source_dir, 'backend'),
     # os.path.dirname(get_python_inc()),
-    numpy.get_include(),
+    # numpy.get_include(),
 ]
 extensions = [
     Extension(
         'waterz.evaluate',
-        sources=['waterz/evaluate.pyx', 'waterz/frontend_evaluate.cpp'],
+        sources=['waterz/evaluate.cpp', 'waterz/frontend_evaluate.cpp'],
         include_dirs=include_dirs,
         language='c++',
         extra_link_args=['-std=c++11'],
@@ -41,6 +52,8 @@ setup(
     author='Jan Funke, Jingpeng Wu',
     author_email='jfunke@iri.upc.edu',
     license='MIT',
+    cmdclass={'build_ext': build_ext},
+    setup_requires=['numpy'],
     install_requires=requirements,
     tests_require=['pytest'],
     packages=find_packages(),
@@ -56,7 +69,7 @@ setup(
     },
     include_package_data=True,
     zip_safe=False,
-    ext_modules=cythonize(extensions),
+    ext_modules=extensions,
     classifiers=[
         "Programming Language :: Python :: 3",
         "Intended Audience :: Developers",
