24067a365248f049cafb5917179b7383b9941ed1
jingpengw <jingpeng.wu@gmail.com>
2019-08-16 16:11:47 -0400

    remove sudo

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -2,20 +2,20 @@
 set -e -x
 
 # Install a system package required by our library
-sudo apt install -y libboost-dev
+apt install -y libboost-dev
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    sudo "${PYBIN}/pip" install -r /io/requirements.txt
-    sudo "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+    "${PYBIN}/pip" install -r /io/requirements.txt
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
-    sudo auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+    auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
 done
 
 # Install packages and test
 for PYBIN in /opt/python/*/bin/; do
-    sudo "${PYBIN}/pip" install waterz --no-index -f /io/wheelhouse
+    "${PYBIN}/pip" install waterz --no-index -f /io/wheelhouse
 done
