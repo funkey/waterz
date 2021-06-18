a4d22e3b577d2682336d24dd8cc5883429a7b2c9
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2021-06-18 13:51:57 +0100

    Replace __builtins__ with builtins
    __builtins__ is a dict, not a module. __builtin__ was the module to use
    in py2. builtins is the module to use in py3.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools.extension import Extension
 from setuptools.command.build_ext import build_ext as _build_ext
 # from Cython.Build import cythonize
 import os
+import builtins
 
 VERSION = '0.9.5'
 
@@ -24,7 +25,7 @@ class build_ext(_build_ext):
     def finalize_options(self):
         _build_ext.finalize_options(self)
         # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
+        builtins.__NUMPY_SETUP__ = False
         import numpy
         self.include_dirs.append(numpy.get_include())
 
